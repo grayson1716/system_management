178527bcefb5d0ca5968f543ffc099ac5cfd7ddf
grayson1716 <60379800+grayson1716@users.noreply.github.com>
2020-11-11 10:18:35 -0800

    Uploaded package_manage.py
    package_manage.py is the first file uploaded.  It is a basic file, which redundantly updates and upgrades packages, and automatically cleans directories.

---
+++ package_manage.py
@@ -0,0 +1,10 @@
+import os as os
+
+os.system('sudo apt-get update')
+os.system('sudo apt update')
+os.system('sudo apt-get upgrade')
+os.system('sudo apt upgrade')
+os.system('sudo apt-get autoremove')
+os.system('sudo apt autoremove')
+os.system('sudo rm -rf ~/.thumbnails')
+os.system('sudo apt-get clean')
