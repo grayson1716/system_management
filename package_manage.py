b26461d974f68efa8686e4645c756cc44aeb2c71
Grayson <grayson1716@hotmail.com>
2020-11-11 11:50:18 -0800

    Improved formatting and completed update messages

--- package_manage.py
+++ package_manage.py
@@ -3,9 +3,15 @@ import os as os
 print('\nFetching Updates...')
 os.system('sudo apt-get update')
 os.system('sudo apt update')
+
+print('\nUpgrading...')
 os.system('sudo apt-get upgrade')
 os.system('sudo apt upgrade')
+
+print('\nAutoremoving...')
 os.system('sudo apt-get autoremove')
 os.system('sudo apt autoremove')
+
+print('\nRemoving Thumbnails and Cleaning...')
 os.system('sudo rm -rf ~/.thumbnails')
 os.system('sudo apt-get clean')
